f540f8f4c68e1dbbc1310f931b1ee74f38dc1c07
Precious Mozia <chibundumozia@gmail.com>
2026-02-21 19:58:14 +0200

    Linux

--- nm_objdump/elf_utils.h
+++
@@ -1,6 +0,0 @@
-#ifndef ELF_UTILS_H
-#define ELF_UTILS_H
-
-void process_file(char *filename);
-
-#endif
--- nm_objdump/hnm.c
+++ nm_objdump/hnm.c
@@ -1,19 +1,60 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include "elf_utils.h"
+#include "hnm.h"
 
-int main(int argc, char **argv)
+
+/**
+* main- func
+* @ac: int
+* @av: char **
+* Return: int
+*/
+int main(int ac, char **av)
 {
-    int i;
+	int fd = 0, i = 1, rv = 0;
 
-    if (argc < 2)
-    {
-        fprintf(stderr, "Usage: %s <objfile ...>\n", argv[0]);
-        return (1);
-    }
+	if (ac == 1)
+	{
+		fd = open("a.out", O_RDONLY);
+		rv = elf_handler(fd);
+		if (rv == 16)
+			fprintf(stderr, "%s: %s: no symbols", av[0], av[1]);
+		close(fd);
+	}
+	else
+	{
+		for (; i < ac; i++)
+		{
+			fd = open(av[i], O_RDONLY);
+			if (fd != -1)
+			{
+				rv = elf_handler(fd);
+				if (rv == 16)
+					fprintf(stderr, "%s: %s: no symbols\n", av[0], av[i]);
+				close(fd);
+			}
+		}
+	}
+	return (EXIT_SUCCESS);
+}
 
-    for (i = 1; i < argc; i++)
-        process_file(argv[i]);
 
-    return (0);
+/**
+* elf_handler- func
+* @fd: int
+* Return: int
+*/
+int elf_handler(int fd)
+{
+	char head[16];
+	int rv = 0;
+
+	if (read(fd, head, (sizeof(char) * 16)) != 16)
+		return (1);
+	if ((head[0] != 0x7F) || (head[1] != 'E') ||
+		(head[2] != 'L') || (head[3] != 'F'))
+		return (1);
+	if (head[4] == ELFCLASS32)
+		rv = manage_32(fd);
+	if (head[4] == ELFCLASS64)
+		rv = manage_64(fd);
+	return (rv);
 }
